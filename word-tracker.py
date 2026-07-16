749160fcb75793e23a6973daf9c41daf5fd53cb8
james1198 <jblee@batestech.edu>
2026-07-16 09:35:22 -0700

    first commit

---
+++ word-tracker.py
@@ -0,0 +1,13 @@
+#this needs to prompt the user for a sentence .
+#it will need to convert all the words to lowercase, delete all punctuation before counting the words, and delete any leading or trailing whitespace.
+#then it will make a list of all indivaidual words in the sentence and print that list out without repetitions while still counting it for the total number of words.
+#with that list it will find the avrage length of the words and the total number of words in the sentence.
+#finaly it will display it clearly to the user in a nice format.
+
+sentence = input("Please enter a sentence or paragraph: ").strip().lower()
+sentence = sentence.translate(str.maketrans('', '', '.,!?;:'))
+all_words = sentence.split()
+unique_words = list(set(sentence.split()))
+print("Words in the sentence:", unique_words)
+print("Average word length:", sum(len(word) for word in all_words) / len(all_words))
+print("Total number of words:", len(all_words))
