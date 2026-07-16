f22d11338cf8b4cf8bd29f8edfd6e372f1baa97e
james1198 <jblee@batestech.edu>
2026-07-16 09:41:00 -0700

    commit

--- word-tracker.py
+++ word-tracker.py
@@ -3,9 +3,10 @@
 #then it will make a list of all indivaidual words in the sentence and print that list out without repetitions while still counting it for the total number of words.
 #with that list it will find the avrage length of the words and the total number of words in the sentence.
 #finaly it will display it clearly to the user in a nice format.
+import string
 
 sentence = input("Please enter a sentence or paragraph: ").strip().lower()
-sentence = sentence.translate(str.maketrans('', '', '.,!?;:'))
+sentence = sentence.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation)))
 all_words = sentence.split()
 unique_words = list(set(sentence.split()))
 print("Words in the sentence:", unique_words)
